99bf1dee74eaccb2318335798bf2f32af5d7fd5c
Youssef Zidan <yz12345678900987654321@gmail.com>
2026-04-04 20:08:03 +0200

    Update MovieCard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,21 @@
+// MovieCard.jsx
 import React from 'react';
 
 const MovieCard = ({ movie: {title, vote_average, poster_path, release_date, original_language} }) => {
     return (
         <div className="movie-card">
-            <img src={poster_path ? `https://image.tmdb.org/t/p/w500${poster_path}` : '/no-movie.png'} alt={title}/>
+            <img 
+                src={poster_path ? `https://image.tmdb.org/t/p/w342${poster_path}` : '/no-movie.png'} 
+                alt={title}
+                width="342"
+                height="513"
+                loading="lazy"
+            />
             <div className="mt-4">
                 <h3 className="text-white">{title}</h3>
                 <div className="content">
                     <div className="rating">
-                        <img src="./star.svg" alt="rating" />
+                        <img src="./star.svg" alt="rating icon" width="16" height="16" />
                         <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
                     </div>
                     <span>•</span>
@@ -21,4 +28,4 @@ const MovieCard = ({ movie: {title, vote_average, poster_path, release_date, ori
     );
 };
 
-export default MovieCard;   
+export default MovieCard;
